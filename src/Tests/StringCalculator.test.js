1b681cf62e06d3abd58b19d1b87a5eeafd35b77e
Anand Yadav <anand@vectorconsulting.in>
2024-12-28 20:50:39 +0530

    Add support for single and two numbers and handle new lines as separators

--- src/Tests/StringCalculator.test.js
+++ src/Tests/StringCalculator.test.js
@@ -4,5 +4,11 @@ describe('string calculator',()=>{
     test('Should return 0 for empty string',()=>{
         expect(add("")).toBe(0)
     });
+    test('should return the sum of two numbers separated by a comma', () => {
+        expect(add("1,5")).toBe(6);
+    });
+    test('should handle new lines as separators', () => {
+        expect(add("1\n2,3")).toBe(6);
+    });
     
 })
--- src/components/StringCalculator.js
+++ src/components/StringCalculator.js
@@ -2,5 +2,6 @@
 
 export function add(numbers){
     if (numbers==='') return 0
-    
+    const num=numbers.split(/[\n,]/).map(Number)
+    return num.reduce((sum,current)=>sum+current,0)
 }
